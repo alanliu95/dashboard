410b5d085dbca2ee415dc30ed9ac811fb7abbaba
alan <438162498@qq.com>
2019-03-18 21:29:31 +0800

    Initial commit

---
+++ src/main/java/com/alan/dashboard/comtroller/HomeController.java
@@ -0,0 +1,18 @@
+package com.alan.dashboard.comtroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(Model model){
+        model.addAttribute("name","alan");
+        return "home";
+    }
+}
+ //@RequestMapping(method= RequstMethod.GET)
+//查询参数 @RequestParam(value="",defaultValue=  )
+//路径变量 占位符 @RequestMapping(value="/{spittleId}",method= RequstMethod.GET)
+//  @PathVariable("spittleId")

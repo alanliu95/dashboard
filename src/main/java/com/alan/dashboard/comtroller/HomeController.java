bcaec61d4e5e9c7d5a310157b8c4dd57f5a83123
alan <438162498@qq.com>
2019-03-23 23:05:54 +0800

    学习bootstrap ajax

--- src/main/java/com/alan/dashboard/comtroller/HomeController.java
+++ src/main/java/com/alan/dashboard/comtroller/HomeController.java
@@ -3,6 +3,9 @@ package com.alan.dashboard.comtroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 @Controller
 public class HomeController {
@@ -10,6 +13,15 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("name","alan");
         return "home";
+    }
+    @RequestMapping("/test")
+    public String test(Model model){
+        return "test";
+    }
+    @RequestMapping("/data")
+    @ResponseBody
+    public String test(){
+        return ""+new Date();
     }
 }
  //@RequestMapping(method= RequstMethod.GET)

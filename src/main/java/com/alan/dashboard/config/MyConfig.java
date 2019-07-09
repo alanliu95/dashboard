54a4d039fe0ebddf6f301e3b3676bd423fa5b58c
alan <438162498@qq.com>
2019-07-09 15:18:00 +0800

    实时显示，历史查询 两个页面可分别单独工作

--- src/main/java/com/alan/dashboard/config/MyConfig.java
+++ src/main/java/com/alan/dashboard/config/MyConfig.java
@@ -10,12 +10,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @ImportResource(locations={"classpath:beans.xml"})
 public class MyConfig {
-    @Autowired
-    private RestTemplateBuilder builder;
-    @Bean
-    public RestTemplate getClient(){
-        System.out.println("test getClient");
-        System.out.println(builder);
-        return this.builder.build();
-    }
+//    @Autowired
+//    private RestTemplateBuilder builder;
+//    @Bean
+//    public RestTemplate getClient(){
+//        System.out.println("test getClient");
+//        System.out.println(builder);
+//        return this.builder.build();
+//    }
 }
